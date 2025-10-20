4a02cbeec93ad16b7ebf840b7f80e4250acc5bff
devkamogelo <99497643+devkamogelo@users.noreply.github.com.>
2025-10-20 23:17:08 +0200

    added a random color background on hover

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ function drawGrid(gridSize){
 
 const container = document.querySelector("#container");
 
+function getRandomInt(){
+    min = 0;
+    max = 255;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
 for(let i = 1; i <= gridSize; i++){
     const line = document.createElement("div");
     line.style.display = 'flex';
     line.style.flex = "1";
     
     for(let k = 1; k <= gridSize; k++){
+        const redValue = getRandomInt();
+        const blueValue = getRandomInt();
+        const greenValue = getRandomInt();
         const star = document.createElement("div");
         star.style.border = "1px solid black";
         star.style.flex = "1";
         star.addEventListener('mouseover', () => {
-            star.style.backgroundColor = "black";
+            star.style.backgroundColor = `rgb(${redValue}, ${blueValue}, ${greenValue})`;
         })
         line.appendChild(star);
     }
